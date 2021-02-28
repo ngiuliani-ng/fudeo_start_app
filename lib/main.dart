6adf0651276adbb97f4bf8c57a5fc61d2d5c3f20
Nicolò Giuliani <info@ngiuliani.com>
2021-02-28 19:52:43 +0100

    Lesson: Stateless vs Stateful.

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,78 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  void onPressed() {
-    print("onPressed");
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carte di Credito"),
+      ),
+      body: Column(
+        children: [
+          BankView(bank: "N26"),
+          BankView(bank: "Hype"),
+        ],
+      ),
+    );
+  }
+}
+
+class BankView extends StatefulWidget {
+  BankView({
+    @required this.bank,
+  });
+  final String bank;
+
+  @override
+  _BankViewState createState() => _BankViewState();
+}
+
+class _BankViewState extends State<BankView> {
+  bool active = false;
+
+  void toggleCard() {
+    setState(() {
+      active = !active;
+    });
+    print("Bank State: $active");
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("App - Flutter Start"),
-        ),
-        body: RaisedButton(
-          child: Text(
-            "My Raised Button",
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Icon(Icons.credit_card),
+          SizedBox(
+            width: 16,
+          ),
+          Text(
+            widget.bank,
             style: TextStyle(
-              color: Colors.blue,
               fontWeight: FontWeight.bold,
             ),
           ),
-          onPressed: this.onPressed,
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: this.onPressed,
-        ),
+          Expanded(
+            child: SizedBox(),
+          ),
+          FlatButton(
+            onPressed: toggleCard,
+            child: Text(active ? "Disattiva" : "Attiva"),
+          ),
+        ],
       ),
+      color: active ? Colors.white : Colors.grey.shade400,
     );
   }
 }
