f846d2e3d9cd82fb1ac8d12c22e14a8881638689
Nicolò Giuliani <info@ngiuliani.com>
2021-02-28 14:50:38 +0100

    End of introduction.

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,30 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  void onPressed() {
+    print("onPressed");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("App - Flutter Start"),
+        ),
+        body: RaisedButton(
+          child: Text(
+            "My Raised Button",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onPressed: this.onPressed,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: this.onPressed,
         ),
       ),
     );
